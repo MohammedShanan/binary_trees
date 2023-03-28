947b727e9b2393f82256eb7ee876145e52039ce9
mohammedshanan <mohammedshanan12@gmail.com>
2023-03-28 19:50:33 +0200

    fixed lowest commond ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,35 +1,59 @@
 #include "binary_trees.h"
 /**
+ * findLCA - Finds the lowest common ancestor of two nodes.
+ * @root: Pointer root node.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
+ * Return: If no common ancestors return NULL, else return common ancestor.
+ */
+binary_tree_t *findLCA(binary_tree_t *root,
+    const binary_tree_t *first,
+    const binary_tree_t *second)
+{
+binary_tree_t *left_lca, *right_lca;
+if (root == NULL)
+{
+return (NULL);
+}
+
+if (root == first || root == second)
+{
+return (root);
+}
+left_lca = findLCA(root->left, first, second);
+right_lca = findLCA(root->right, first, second);
+if (left_lca && right_lca)
+{
+return (root);
+}
+return (left_lca ? left_lca : right_lca);
+}
+/**
+ * get_root - Finds the root node.
+ * @first: Pointer to a node to start search from.
+ * Return: The root node.
+ */
+
+binary_tree_t *get_root(binary_tree_t *node)
+{
+if (node->parent == NULL)
+{
+return (node);
+}
+return (get_root(node->parent));
+}
+/**
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
  * @first: Pointer to the first node.
  * @second: Pointer to the second node.
  * Return: If no common ancestors return NULL, else return common ancestor.
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-            const binary_tree_t *second)
+                    const binary_tree_t *second)
 {
-binary_tree_t *first_anc, *second_anc;
 if (!first || !second)
 {
 return (NULL);
 }
-first_anc = first->parent;
-second_anc = second->parent;
-if (first == second_anc || !first_anc)
-{
-return ((binary_tree_t *)first);
-}
-else if (second == first_anc || !second_anc)
-{
-return ((binary_tree_t *)second);
-}
-else if (first_anc->parent == NULL)
-{
-return (first_anc);
-}
-else if (second_anc->parent == NULL)
-{
-return (second_anc);
-}
-return (binary_trees_ancestor(first_anc, second_anc));
+return (findLCA(get_root((binary_tree_t *)first), first, second));
 }
---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,4 @@
+#include "binary_trees.h"
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+}
