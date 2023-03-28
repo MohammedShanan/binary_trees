22fff57763178327a484544699c7e4e9a0a06ee5
mohammedshanan <mohammedshanan12@gmail.com>
2023-03-28 20:18:48 +0200

    fixed lowest commond ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -51,5 +51,11 @@ return (get_root(node->parent));
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second)
 {
+
+if (!first || !second)
+{
+return (NULL);
+}
+
 return (findLCA(get_root((binary_tree_t *)first), first, second));
 }
