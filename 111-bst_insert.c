ae0c1941bcdcdeea3e95c2b5de4a7be25cd77eae
mohammedshanan <mohammedshanan12@gmail.com>
2023-03-29 19:01:31 +0200

    solved bst insert

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -8,38 +8,38 @@
 
 bst_t *bst_insert(bst_t **tree, int value)
 {
-    bst_t *current, *new_node;
-    if (tree)
-    {
-        current = *tree;
-        if (*tree == NULL)
-        {
-            new_node = binary_tree_node(current, value);
-            if (!new_node)
-                return (NULL);
-            *tree = new_node;
-        }
-        else if (current->n > value)
-        {
-            if (current->left)
-                return (bst_insert(&current->left, value));
-            new_node = binary_tree_node(current, value);
-            if (!new_node)
-                return (NULL);
-            current->left = new_node;
-        }
-        else if (current->n < value)
-        {
-            if (current->right)
-                return (bst_insert(&current->right, value));
-            new_node = binary_tree_node(current, value);
-            if (!new_node)
-                return (NULL);
-            current->right = new_node;
-        }
-        else
-            return (NULL);
-        return (new_node);
-    }
-    return (NULL);
+bst_t *current, *new_node;
+if (tree)
+{
+current = *tree;
+if (*tree == NULL)
+{
+new_node = binary_tree_node(current, value);
+if (!new_node)
+return (NULL);
+*tree = new_node;
+}
+else if (current->n > value)
+{
+if (current->left)
+return (bst_insert(&current->left, value));
+new_node = binary_tree_node(current, value);
+if (!new_node)
+return (NULL);
+current->left = new_node;
+}
+else if (current->n < value)
+{
+if (current->right)
+return (bst_insert(&current->right, value));
+new_node = binary_tree_node(current, value);
+if (!new_node)
+return (NULL);
+current->right = new_node;
+}
+else
+return (NULL);
+return (new_node);
+}
+return (NULL);
 }
