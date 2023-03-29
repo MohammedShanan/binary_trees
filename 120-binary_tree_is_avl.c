9488d4918ef2eba126026458e67941871e87e6b0
mohammedshanan <mohammedshanan12@gmail.com>
2023-03-29 22:49:18 +0200

    solved is avl

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -7,15 +7,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t height_l, height_r;
-    height_l = height_r = 0;
-    if (tree)
-    {
-        height_l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-        height_r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-        return (height_l > height_r ? height_l : height_r);
-    }
-    return (0);
+size_t height_l, height_r;
+height_l = height_r = 0;
+if (tree)
+{
+height_l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+height_r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+return (height_l > height_r ? height_l : height_r);
+}
+return (0);
 }
 /**
  * is_avl - Checks if a binary tree is a valid AVL tree.
@@ -26,25 +26,25 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int is_avl(const binary_tree_t *tree, int lo, int hi)
 {
-    size_t r_height, l_height;
-    int diff;
-    if (tree)
-    {
-        if (tree->n <= lo || tree->n >= hi)
-        {
-            return (0);
-        }
-        r_height = binary_tree_height(tree->left);
-        l_height = binary_tree_height(tree->right);
-        diff = r_height - l_height;
-        if (diff > 1 || diff < -1)
-        {
-            return (0);
-        }
-        return (is_avl(tree->left, lo, tree->n) &&
-                is_avl(tree->right, tree->n, hi));
-    }
-    return (1);
+size_t r_height, l_height;
+int diff;
+if (tree)
+{
+if (tree->n <= lo || tree->n >= hi)
+{
+return (0);
+}
+r_height = binary_tree_height(tree->left);
+l_height = binary_tree_height(tree->right);
+diff = r_height - l_height;
+if (diff > 1 || diff < -1)
+{
+return (0);
+}
+return (is_avl(tree->left, lo, tree->n) &&
+is_avl(tree->right, tree->n, hi));
+}
+return (1);
 }
 /**
  * binary_tree_is_avl - Checks if a binary tree is a valid AVL tree.
@@ -53,9 +53,9 @@ int is_avl(const binary_tree_t *tree, int lo, int hi)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    return (is_avl(tree, INT_MIN, INT_MAX));
+if (tree == NULL)
+{
+return (0);
+}
+return (is_avl(tree, INT_MIN, INT_MAX));
 }
