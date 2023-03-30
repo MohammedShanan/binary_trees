160e1381e923182e803e42dfd4e014118d9d1b14
mohammedshanan <mohammedshanan12@gmail.com>
2023-03-30 19:51:46 +0200

    solved array to max heap

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -3,7 +3,7 @@
  * array_to_heap - Builds a max heap from an array.
  * @array: A pointer to the first element of the array to be converted.
  * @size: The number of elements in @array.
- * Return: A pointer to the root node of the created heap, or NULL upon failure.
+ * Return: A pointer to the root node of the created heap, or NULL upon failure
  */
 heap_t *array_to_heap(int *array, size_t size)
 {
