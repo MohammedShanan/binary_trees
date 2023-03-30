bdc4921f57c0d494df0e1db6be324b5a5e6137cb
mohammedshanan <mohammedshanan12@gmail.com>
2023-03-30 22:14:00 +0200

    solved heao sort

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -9,8 +9,19 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
 int *array;
 size_t i;
+if (heap == NULL)
+{
+    *size = 0;
+    return (NULL);
+}
 *size = _binary_tree_size(heap);
 array = malloc(sizeof(int) * (*size));
+if (!array)
+{
+    *size = 0;
+    return (NULL);
+}
+
 for (i = (*size); i >= 1; i--)
 {
 array[i - 1] = heap_extract(&heap);
