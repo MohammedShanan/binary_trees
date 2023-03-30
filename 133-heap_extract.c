0019f1aa35b640429ad9d0e3270e45f43eaab7d4
mohammedshanan <mohammedshanan12@gmail.com>
2023-03-30 20:56:05 +0200

    solved extract heap

---
+++ 133-heap_extract.c
@@ -0,0 +1,115 @@
+#include "binary_trees.h"
+
+/**
+ * heapify - heapify a subtree .
+ * @root: pointer to the root of the heap
+ */
+
+void heapify(heap_t *root)
+{
+if (root && (root->right || root->left))
+{
+if (root->left && root->left->n > root->n)
+{
+if (root->right == NULL || root->left->n > root->right->n)
+{
+swap(&root->n, &root->left->n);
+heapify(root->left);
+}
+else
+{
+swap(&root->n, &root->right->n);
+heapify(root->right);
+}
+}
+else if (root->right && root->right->n > root->n)
+{
+swap(&root->n, &root->right->n);
+heapify(root->right);
+}
+}
+}
+
+/**
+ * get_last_node - get the last node of the heap.
+ * @root: pointer to the root of the heap
+ * @indx: indx of the current node
+ * @last_indx: indx of the last node
+ * Return: the last node in the heap.
+ */
+
+heap_t *get_last_node(heap_t *root, size_t indx, size_t last_indx)
+{
+heap_t *l_path, *r_path;
+if (indx == last_indx)
+{
+return (root);
+}
+if (root)
+{
+l_path = get_last_node(root->left, 2 * indx + 1, last_indx);
+r_path = get_last_node(root->right, 2 * indx + 2, last_indx);
+if (l_path)
+{
+if (root->left == l_path)
+{
+root->left = NULL;
+}
+return (l_path);
+}
+else if (r_path)
+{
+if (root->right == r_path)
+{
+root->right = NULL;
+}
+return (r_path);
+}
+}
+return (NULL);
+}
+/**
+ * heap_extract - extracts the root node of a Max Binary Heap.
+ * @root: a double pointer to the root of the heap
+ * Return: the root node number or 0.
+ */
+int heap_extract(heap_t **root)
+{
+heap_t *last_node;
+size_t last_node_indx;
+int max_num;
+if (*root == NULL)
+{
+return (0);
+}
+max_num = (*root)->n;
+last_node_indx = binary_tree_size(*root) - 1;
+last_node = get_last_node(*root, 0, last_node_indx);
+(*root)->n = last_node->n;
+free(last_node);
+heapify(*root);
+return (max_num);
+}
+
+/**
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: A pointer to the parent of the node to create.
+ * @value: The value to put in the new node.
+ * Return: If an error occurs - NULL.
+ * Otherwise - a pointer to the new node.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+binary_tree_t *new_node;
+new_node = malloc(sizeof(binary_tree_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = value;
+new_node->left = NULL;
+new_node->right = NULL;
+new_node->parent = parent;
+
+return (new_node);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -72,4 +72,6 @@ avl_t *avl_insert(avl_t **tree, int value);
 int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+void swap(int *a, int *b);
 #endif
